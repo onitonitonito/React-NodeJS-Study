ec952157d2104f32fd2722c99f08b63fe9e6f7b3
Kay Suparx <nitt0x0@gmail.com>
2019-12-01 10:56:19 +0900

    4.2-rolling react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+/*
+* App(조립) 을 받아서 랜더링(index) 만 해준다.
+* -------------
+*
+* components(부품) --> App(조립) --> index(DOM 랜더링)
+* 구조가 간단하면 바로, App(조립) --> index(DOM 랜더링)
+**/
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './_static/serviceWorker';
+import ReactDom from 'react-dom';
 
-import App from './components/CodeLabApp';
+// import App from './components/CounterOld';
+import App from './components/FirstDeployApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(<App/>, document.getElementById('root'));
+
+
+
+/*
+APP 불러와서 랜더링하기
+
+(1) import ReacDom / ReacDom.render() 취소
+(2) export defaul App; 추가
+(3) dir변경 ./componets --> ../componets
+
+**/
